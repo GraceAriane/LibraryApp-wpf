9e742691032bd56f3308e7020146c14060784e3a
GraceAriane <tchoukeugrace@gmail.com>
2026-03-29 08:45:43 +0100

    fix: ignore build files

--- obj/Debug/net10.0-windows/LibraryApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/LibraryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18315d99b6c50dcb89005a8fbaf64a803db1964c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00577311bf3757403cd7fb5c6da4307747838019")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
